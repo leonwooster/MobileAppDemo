3f0d7ce7b6f3d11756f939f8124c2919f36a8643
Ashish Agrawal <Ashish.Agrawal@bcstechnology.com.au>
2020-06-22 16:00:35 +0530

    test

--- MobileApp/UITest/Pages/HomePage.cs
+++ MobileApp/UITest/Pages/HomePage.cs
@@ -11,25 +11,25 @@ namespace UITest.Pages
 {
     class HomePage
     {   
-        public Query btnMore => x => x.Marked("More options");
+       // public Query btnMore => x => x.Marked("More options");
         public Query btnAdd => x => x.Text("Add");
-        public Query txtEA => x => x.Text("EA");
+       // public Query txtEA => x => x.Text("EA");
 
 
         internal void ClickAdd()
         {
-            Settings.AppContext.Tap(btnMore);
+           // Settings.AppContext.Tap(btnMore);
             Settings.AppContext.Tap(btnAdd);
         }
 
         internal void WaitForListElement()
         {
-            Settings.AppContext.WaitForElement(txtEA);
+          //  Settings.AppContext.WaitForElement(txtEA);
         }
 
-        internal int GetElementCount()
-        {
-            return Settings.AppContext.Query(x => x.Id("recyclerView").All().Text("EA")).Count();
-        }
+        //internal int GetElementCount()
+        //{
+        //   // return Settings.AppContext.Query(x => x.Id("recyclerView").All().Text("EA")).Count();
+        //}
     }
 }
